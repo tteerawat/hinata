d0e336fc864d09cba84b9076727d9b55d2b62626
teerawat1992 <teerawat.lamanchart@gmail.com>
2020-10-11 01:16:08 +0700

    Edit mix.exs for package release

--- mix.exs
+++ mix.exs
@@ -1,12 +1,17 @@
 defmodule Hinata.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+
   def project do
     [
       app: :hinata,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.11",
+      build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
+      description: description(),
+      package: package(),
       deps: deps()
     ]
   end
@@ -16,6 +21,16 @@ defmodule Hinata.MixProject do
     [
       extra_applications: [:logger],
       mod: {Hinata.Application, []}
+    ]
+  end
+
+  defp description do
+    "An HTTP client for JSON API."
+  end
+
+  defp package do
+    [
+      files: ~w(lib priv .formatter.exs mix.exs README*)
     ]
   end
 
